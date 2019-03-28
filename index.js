924241ed678c5c2ed9f5c308e99a42f8962bec5d
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-28 12:39:20 -0700

    knex initialized, dbConfig

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const server = express();
+
 const PORT = process.env.PORT || 4000;
+const db = require("./db/dbConfig");
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json(`Welcome to my recipe book`);
+});
 
 server.listen(PORT, () => console.log(`\n Server is live on Port ${PORT}\n`));
