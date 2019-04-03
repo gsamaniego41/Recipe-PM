bc2133405b46a0b89009dca434e2d318a1267cc7
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-04-02 23:41:04 -0700

    renders list of dishes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    dishes: []
+  };
+
+  componentDidMount = () => {
+    axios
+      .get("http://localhost:4000/dishes")
+      .then(res => this.setState({dishes: res.data}))
+      .catch(err => console.log(err.msg));
+  };
+
   render() {
     return (
       <div>
         <h1>Recipe Book</h1>
+        {this.state.dishes.map(dish => (
+          <h2>{dish.name}</h2>
+        ))}
       </div>
     );
   }
