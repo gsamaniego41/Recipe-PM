82e63bbffaf0532e5279a760b9a13950ac24ba9e
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-04-02 23:31:44 -0700

    adds cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const dishesRouter = require("./routes/dishesRouter");
 const recipesRouter = require("./routes/recipesRouter");
@@ -11,6 +12,7 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
 
 server.use("/dishes", dishesRouter);
 server.use("/recipes", recipesRouter);
